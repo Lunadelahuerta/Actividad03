3988dac2c636f5932b1939f3ff8e3b4dc83a0d71
Luna <luna.huerta.c@gmail.com>
2017-04-07 20:30:50 -0300

    agrego cambios

--- main.js
+++ main.js
@@ -16,9 +16,8 @@ var dic = " diciembre"
 var meses = [ene, feb, mar, abr, may, jun, jul, ago, sep, oct, nov, dic]
 
 for (var i = 0; i < meses.length; i++){
-    console.log(cumple[i])
-    if (cumple != meses){
-    alert("Bacán, puedes celebrar tu no cumpleaños estos meses! " + meses)
+    if (cumple != meses[i]){
+    console.log(meses[i])
   }
 }
 
